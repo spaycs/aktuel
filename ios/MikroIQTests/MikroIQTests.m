85a992a1c178b79de994401e059611c6b8ddbbc2
spaycs <132484947+spaycs@users.noreply.github.com>
2024-11-07 20:05:15 +0300

    Çakışmalar çözüldü ve kod güncellendi

--- ios/MikroIQTests/MikroIQTests.m
+++ ios/MikroIQTests/MikroIQTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MikroIQTests : XCTestCase
+@interface mikroiqTests : XCTestCase
 
 @end
 
-@implementation MikroIQTests
+@implementation mikroiqTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
