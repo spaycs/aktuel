6e89c769d4e2cd328ad93b842cd78665faf3aa72
spaycs <132484947+spaycs@users.noreply.github.com>
2024-12-12 21:21:03 +0300

    mikroiq

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import VersionCheck from 'react-native-version-check';
 
 function App() {
     const [isUpdateRequired, setIsUpdateRequired] = useState(false);
-{/* 
+{/*  
     useEffect(() => {
         const checkAppVersion = async () => {
             try {
