beda53a2618b4775993510c6a87d928d511dba58
spaycs <132484947+spaycs@users.noreply.github.com>
2025-01-23 13:47:01 +0300

    mikroiq

--- src/screens/AlinanSiparisOnizleme/index.js
+++ src/screens/AlinanSiparisOnizleme/index.js
@@ -490,7 +490,7 @@ useEffect(() => {
           {
             evrak_aciklamalari: formatExplanations(),
             satirlar: productsWithCalculatedValues.map((product) => ({
-              sip_tarih: '23.01-2025',
+              sip_tarih: '23.01.2024',
               sip_tip: alinanSiparis.sip_tip,
               sip_cins: alinanSiparis.sip_cins,
               sip_evrakno_seri: alinanSiparis.sip_evrakno_seri,
