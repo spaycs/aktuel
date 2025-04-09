63a92dbc21d852c2525cc709cda812305379519d
spaycs <132484947+spaycs@users.noreply.github.com>
2025-04-09 13:29:59 +0300

    aktuel

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
                         </AuthDefaultProvider>
                     </AuthProvider>
                 </NavigationContainer>
-        </GestureHandlerRootView>
+        </GestureHandlerRootView> 
     );
 }
 
